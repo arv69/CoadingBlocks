ab8cc78eaca5071c315778093adece574b996661
arv69 <62993813+arv69@users.noreply.github.com>
2023-12-20 20:27:35 +0530

    Update 8_InbuiltSort.cpp
    we have to give one index extra 
    arr, arr+n  
    actual it is sorting arr, arr+n-1 
    but we are giving arr+n +1  extra index
    sort  [start, end )

--- Arrays/8_InbuiltSort.cpp
+++ Arrays/8_InbuiltSort.cpp
@@ -12,8 +12,8 @@ for(int i=0;i<10;i++){
     cin>>arr[i];
 }
 int n=10;
-// Note that [start ,end) last-1
-sort(arr,arr+n-1);  
+// Note that [start ,end)  arr+n-1  but we are giving a arr+n one index extra
+sort(arr,arr+n);  
    for(int i=0;i<n;i++){
     cout<<arr[i]<<",";
    }
