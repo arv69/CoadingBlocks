8711d151aadc4dca8c715abccc1b4f4c47beddbc
arv69 <62993813+arv69@users.noreply.github.com>
2023-12-20 23:37:08 +0530

    Subarray
    what is a subarray
    subarray is a smallar part of a continuous array

---
+++ Arrays/11_SubArray.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+// Genrate all SubArray of a give SubArray
+// what is SubArray?
+// SubArray is the smallest continuous part of an array
+//  array-> 1 2 3 4
+//  SubArray of length 1
+//   1
+//   2
+//   3
+//   4
+// sub array of length 2
+// 1,2
+// 2,3
+// 3,4
+
+// sub array of length 3
+// 1,2,3
+// 2,3,4
+
+// sub array of length 4
+// 1,2,3,4
+
+
+int main()
+{
+   int arr[5];
+   int n=5;
+   for(int i=0;i<n;i++){
+    cin>>arr[i];
+   } 
+
+   //Generate all SubArrays
+
+for(int i=0;i<n;i++){
+    for(int j=i;j<n;j++){
+
+        // Elements of subarray(i,j)
+        for(int k=i;k<=j;k++){
+            cout<<arr[k]<<",";
+        }
+        cout<<endl;
+    }
+}
+    return 0;
+}
